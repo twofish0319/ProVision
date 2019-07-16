e8d804b81cccef9d17906f8f8f9d102e58cdc1e3
黃國豪 <twofish0319@gmail.com>
2019-07-16 15:05:55 +0800

    V3

--- ProVGitTest/Form1.Designer.cs
+++ ProVGitTest/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -38,7 +39,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(223, 97);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Vesion 2";
+            this.button1.Text = "Vesion 3";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
@@ -47,14 +48,24 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(223, 97);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Vesion 2";
+            this.button2.Text = "Vesion 3";
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(238, 240);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(223, 97);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Vesion 3";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -67,6 +78,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
